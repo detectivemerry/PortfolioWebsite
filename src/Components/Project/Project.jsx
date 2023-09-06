92ba79138e32d87346eb0dcadbd47db61a6af24c
detectivemerry <jiiasheng@hotmail.com>
2023-09-06 08:52:42 +0800

    Add animation to project

--- src/Components/Project/Project.jsx
+++ src/Components/Project/Project.jsx
@@ -1,23 +1,60 @@
 import styles from './Project.module.css'
+import { useAnimation, motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+import { useEffect } from "react";
 
-function Project({project}) {
-    const {projectTitle, projectDescription, projectImgUrl, liveDemoLink, githubLink} = project
+
+function Project({ project }) {
+
+    const { projectTitle, projectDescription, projectImgUrl, liveDemoLink, githubLink } = project
+
+    const cardVariants = {
+        visible: { opacity: 1, transition: { duration: 1, delay: 0.2 }, x: 0 },
+        hidden: { opacity: 0, transition: { duration: 1 }, x: -300 },
+    };
+
+    // give us more control with access to helper functions like start/stop
+    const controls = useAnimation();
+
+    // hook to detect if HTML slement with ref is in viewport 
+    const [ref, inView] = useInView();
+
+    useEffect(() => {
+
+        if (inView) {
+            controls.start("visible");
+        }
+        else {
+            controls.start("hidden");
+        }
+
+    }, [controls, inView]);
+
     return (
-        <div className = {styles.projectCard}>
-            <div className = {styles.projectImg}>
-                <img src = {projectImgUrl} />
+
+        <motion.div
+            className={styles.projectCard}
+            ref={ref}
+            animate={controls}
+            initial="hidden"
+            variants={cardVariants}>
+                
+            <div className={styles.projectImg}>
+                <img src={projectImgUrl} />
             </div>
-            <div className = {styles.projectDescriptionContainer}>
-                <div className= {styles.projectDescription}>
-                    <div className = {styles.projectTitle}>{projectTitle}</div>
+            <div className={styles.projectDescriptionContainer}>
+                <div className={styles.projectDescription}>
+                    <div className={styles.projectTitle}>{projectTitle}</div>
                     {projectDescription}
                 </div>
-                <div className = {styles.projectButtons}>
+                <div className={styles.projectButtons}>
                     {githubLink && <button>Github repo</button>}
                     {liveDemoLink && <button>Live demo</button>}
                 </div>
             </div>
-        </div>
+
+        </motion.div>
+
     )
 }
 
--- src/Pages/Projects/Projects.jsx
+++ src/Pages/Projects/Projects.jsx
@@ -1,16 +1,26 @@
 import styles from './Projects.module.css'
 import Project from '../../Components/Project/Project.jsx'
 import SectionHeader from '../../Components/SectionHeader/SectionHeader.jsx'
+import { motion } from "framer-motion";
 
 function Projects({projects}) {
 
   return (
-    <div className = {styles.projects}>
+    <motion.div 
+      className = {styles.projects}
+      >
       <SectionHeader title="Projects" />
         {projects && projects.map((project, idx)=>{
-          return <Project key = {idx} project = {project}/>
+          return <Project key = {idx} project = {project} />
+        //   <motion.Project
+        //   ref={ref}
+        //   animate={{ scale: 2 }}
+        //   initial="hidden"
+        //   variants={squareVariants}
+        //   className="square"
+        // ></motion.Project>
         })}
-    </div>
+    </motion.div>
   )
 }
 
